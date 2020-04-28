4fb67a66c0ace1fcd02d53c9c5c7ee9966989b1f
mdShakilHossainNsu20!8 <shakilnsu2018@gmail.com>
2020-04-28 16:12:17 +0600

    html added

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Choice
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse('hello World')
+
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    return render(request, 'polls/index.html', context=dict(latest_question_list=latest_question_list))
+
+
+def detail(request, question_id):
+    return HttpResponse(f'you are looking for question {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'You\'re looking at the results of question {question_id}')
+
+
+def votes(request, question_id):
+    return HttpResponse(f'You\'re looking at the votes of question {question_id}')
